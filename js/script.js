bdad2b630ad5dc6704b5fc39f7b06e4ac25557d3
Kari Atílio Moreira <202401635278@alunos.estacio.br>
2024-04-28 16:42:11 -0300

    estudo de loops while, arrays, condicionais, template literals

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//estudos de loop while
+//arrays
+//condicionais
+//template literals
+
+
+let option;
+let items = []
+
+while(option != 3) {
+
+    option = Number(prompt(`
+      Olá, usuário! Digite o número da opção desejada:
+      
+      1. Cadastrar um item na lista.
+      2. Mostrar itens cadastrados.
+      3. Sair do programa.
+      `))
+
+    switch(option){
+        case 1:
+            if (option == 1) {
+                let item = prompt("Digite o nome do item");
+            items.push(item)
+            } 
+            break;
+        case 2:
+            if (items.length == 0) {
+                alert("Não existem itens cadastrados");
+            } else {
+                alert(items);
+            }
+            break;
+        case 3:
+            alert("good bye!");  
+             break;
+        default:
+            alert("Opção inválida. Tente novamente!")
+    }
+
+}
+
